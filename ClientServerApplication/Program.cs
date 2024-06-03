6672672bab3d273b9a00ee3d419ce18ea9a7d392
Mad's Residence <Mad's Residence@DESKTOP-974IGT6>
2024-06-03 21:57:00 +0300

    Добавьте файлы проекта.

---
+++ ClientServerApplication/Program.cs
@@ -0,0 +1,78 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+
+public interface IMessageSourceClient
+{
+    event EventHandler<string> MessageReceived;
+    void Connect(string address);
+    void SendMessage(string message);
+    void Disconnect();
+}
+
+public class NetMQClient : IMessageSourceClient
+{
+    private readonly string address;
+    private RequestSocket clientSocket;
+    private NetMQPoller poller;
+    private Thread clientThread;
+
+    public event EventHandler<string> MessageReceived;
+
+    public static void Main(string[] args)
+    {
+        NetMQClient client = new NetMQClient("tcp://localhost:5556");
+        client.Connect("tcp://localhost:5556");
+
+        Console.WriteLine("Подключение к серверу..");
+        Console.WriteLine("Для выхода введите 'exit'");
+
+        while (true)
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+
+            if (message.ToLower() == "exit")
+                break;
+
+            client.SendMessage(message);
+        }
+
+        client.Disconnect();
+    }
+
+    public NetMQClient(string address)
+    {
+        this.address = address;
+    }
+
+    public void Connect(string address)
+    {
+        clientSocket = new RequestSocket();
+        clientSocket.Connect(address);
+
+        poller = new NetMQPoller { clientSocket };
+        clientSocket.ReceiveReady += OnReceiveReady;
+
+        clientThread = new Thread(poller.Run) { IsBackground = true };
+        clientThread.Start();
+    }
+
+    private void OnReceiveReady(object sender, NetMQSocketEventArgs e)
+    {
+        var message = e.Socket.ReceiveFrameString();
+        MessageReceived?.Invoke(this, message);
+    }
+
+    public void SendMessage(string message)
+    {
+        clientSocket.SendFrame(message);
+    }
+
+    public void Disconnect()
+    {
+        poller.Stop();
+        clientSocket.Close();
+        clientSocket.Dispose();
+        poller.Dispose();
+    }
+}
---
+++ ServerApplication/Program.cs
@@ -0,0 +1,66 @@
+﻿using NetMQ.Sockets;
+using NetMQ;
+
+public interface IMessageSource
+{
+    event EventHandler<string> MessageReceived;
+    void SendMessage(string message);
+}
+
+public class NetMQServer : IMessageSource
+{
+    private readonly string address;
+    private NetMQSocket serverSocket;
+    private NetMQPoller poller;
+    private Thread serverThread;
+
+    public event EventHandler<string> MessageReceived;
+
+    public static void Main(string[] args)
+    {
+        NetMQServer server = new NetMQServer("tcp://*:5556");
+        server.Start();
+
+        Console.WriteLine("Сервер запущен!");
+        Console.ReadLine();
+
+        server.Stop();
+    }
+
+    public NetMQServer(string address)
+    {
+        this.address = address;
+    }
+
+    public void Start()
+    {
+        serverSocket = new ResponseSocket();
+        serverSocket.Bind(address);
+
+        poller = new NetMQPoller { serverSocket };
+        serverSocket.ReceiveReady += OnReceiveReady;
+
+        serverThread = new Thread(poller.Run) { IsBackground = true };
+        serverThread.Start();
+    }
+
+    private void OnReceiveReady(object sender, NetMQSocketEventArgs e)
+    {
+        var message = e.Socket.ReceiveFrameString();
+        MessageReceived?.Invoke(this, message);
+        e.Socket.SendFrame("Ack");
+    }
+
+    public void SendMessage(string message)
+    {
+        serverSocket?.SendFrame(message);
+    }
+
+    public void Stop()
+    {
+        poller?.Stop();
+        serverSocket?.Close();
+        serverSocket?.Dispose();
+        poller?.Dispose();
+    }
+}
